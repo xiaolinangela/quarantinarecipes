89f931dd300fb7bb7bc90e469badc07d6ffcf618
Angela Zhu <xiaolinangela@gmail.com>
2020-04-07 21:46:16 -0700

    added redux store

---
+++ quarantinarecipes/frontend/src/components/recipes/RecipeList.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class RecipeList extends Component {
+  state = {};
+  render() {
+    return <div>RecipeList</div>;
+  }
+}
+
+export default RecipeList;
