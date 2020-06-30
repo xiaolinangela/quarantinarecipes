2bddb1c214021318222298213b5525d550cbafe2
Angela Zhu <xiaolinangela@gmail.com>
2020-06-30 14:11:36 -0700

    unicorn

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,34 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: false,
+            },
+          },
+        ],
+      },
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: "file-loader",
+      //       options: {
+      //         outputPath: "img/",
+      //         name: "[name].[ext]",
+      //         publicPath: "/",
+      //       },
+      //     },
+      //   ],
+      // },
     ],
   },
 };
